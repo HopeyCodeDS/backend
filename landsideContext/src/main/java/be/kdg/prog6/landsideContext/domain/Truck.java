5906b764e92b741f1975ad0671c61d70d4bb5e65
Opeyemi Momodu <opeyemimomodu2016@gmail.com>
2024-10-07 02:48:12 +0200

    updated the truck class

--- landsideContext/src/main/java/be/kdg/prog6/landsideContext/domain/Truck.java
+++ landsideContext/src/main/java/be/kdg/prog6/landsideContext/domain/Truck.java
@@ -11,6 +11,9 @@ public class Truck {
     private String warehouseID;
     private LocalDateTime arrivalWindow;
     private MaterialType materialType;
+    private String currentWeighingBridgeNumber; // The weighing bridge number linked to this truck
+    private String assignedConveyorBelt;
+    private boolean weighed;
 
     public Truck(LicensePlate licensePlate, double tareWeight, double grossWeight, String warehouseID, LocalDateTime arrivalWindow, MaterialType materialType) {
         this.licensePlate = licensePlate;
@@ -21,10 +24,30 @@ public class Truck {
         this.materialType = materialType;
     }
 
+    public Truck(LicensePlate licensePlate,LocalDateTime arrivalWindow, MaterialType materialType,String warehouseID) {
+        this.licensePlate = licensePlate;
+        this.arrivalWindow = arrivalWindow;
+        this.materialType = materialType;
+        this.warehouseID = warehouseID;
+        this.currentWeighingBridgeNumber = null;
+        this.assignedConveyorBelt = null;
+
+    }
+
     public Truck(String licensePlate, LocalDateTime arrivalWindow, MaterialType material) {
         this.licensePlate = new LicensePlate(licensePlate);
         this.arrivalWindow = arrivalWindow;
         this.materialType = material;
+    }
+
+    public Truck(LicensePlate licensePlate, String warehouseID, LocalDateTime arrivalWindow, MaterialType materialType, boolean weighed) {
+        this.licensePlate = licensePlate;
+        this.warehouseID = warehouseID;
+        this.arrivalWindow = arrivalWindow;
+        this.materialType = materialType;
+        this.currentWeighingBridgeNumber = null;
+        this.assignedConveyorBelt = null;
+        this.weighed = false;
     }
 
     public String getLicensePlate() {
@@ -58,5 +81,39 @@ public class Truck {
     public WeightBridgeTicket generateWeightBridgeTicket() {
         return null;
     }
+
+    // Methods to assign weighing bridge and conveyor belt
+    public void assignWeighingBridge(String weighingBridgeNumber) {
+        this.currentWeighingBridgeNumber = weighingBridgeNumber;
+    }
+
+    public void assignConveyorBelt(String conveyorBelt) {
+        this.assignedConveyorBelt = conveyorBelt;
+    }
+    public String getCurrentWeighingBridgeNumber() {
+        return currentWeighingBridgeNumber;
+    }
+
+    public String getAssignedConveyorBelt() {
+        return assignedConveyorBelt;
+    }
+
+    public void setCurrentWeighingBridgeNumber(String currentWeighingBridgeNumber) {
+        this.currentWeighingBridgeNumber = currentWeighingBridgeNumber;
+    }
+
+    public void setAssignedConveyorBelt(String assignedConveyorBelt) {
+        this.assignedConveyorBelt = assignedConveyorBelt;
+    }
+
+    // Method to mark the truck as weighed
+    public void markAsWeighed() {
+        this.weighed = true;
+    }
+
+    public boolean isWeighed() {
+        return weighed;
+    }
+
 }
 
