a8dacf5b44f9add6c9b3c15565e6b18d1645c9e3
Opeyemi Momodu <opeyemimomodu2016@gmail.com>
2024-10-07 08:56:50 +0200

    deleted

--- landsideContext/src/main/java/be/kdg/prog6/landsideContext/domain/WeightBridgeTicket.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.prog6.landsideContext.domain;
-
-import java.time.LocalDateTime;
-
-public class WeightBridgeTicket {
-    private LicensePlate licensePlate;
-    private double netWeight;
-    private LocalDateTime dateTime;
-}
